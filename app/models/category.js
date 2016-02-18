c9566b118112c618780801f5deb1fc42e0805efd
cindyk09 <cindy_kim88@hotmail.com>
2016-02-18 18:50:52 -0500

    finished models, templates and routes for all

--- app/models/category.js
+++ app/models/category.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  users: DS.belongsTo('user')
+  users: DS.belongsTo('user', { async: true })
 });
--- app/models/expense.js
+++ app/models/expense.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  amount: DS.attr('float'),
+  amount: DS.attr('number'),
   string: DS.attr('string'),
-  categories: DS.belongsTo('category'),
-  users: DS.belongsTo('user')
+  categories: DS.belongsTo('category', { async: true }),
+  users: DS.belongsTo('user', { async: true })
 });
