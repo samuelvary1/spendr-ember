159e76d4a46cdf1e407b293d1b09bfc91afa58f3
cindyk09 <cindy_kim88@hotmail.com>
2016-02-20 15:28:28 -0500

    fixed application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,11 +5,3 @@ export default ActiveModelAdapter.extend(DataAdapterMixin, {
   namespace: "api/v1",
   authorizer: "authorizer:devise"
 });
-
-
-import DS from 'ember-data';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:devise'
-});
