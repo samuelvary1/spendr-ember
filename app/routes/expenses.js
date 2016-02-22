4fa81c91da46b83c76a4293ac554dc2ce667202d
Sam Vary <sam.vary@gmail.com>
2016-02-22 11:21:23 -0500

    made it a little prettier

--- app/routes/expenses.js
+++ app/routes/expenses.js
@@ -15,6 +15,10 @@ export default Ember.Route.extend( {
     delete(expense) {
       expense.destroyRecord();
       this.transitionTo('expenses.index');
+    },
+
+    totalExpenses(){
+      this.modelFor(this.routeName)
     }
   }
 });
