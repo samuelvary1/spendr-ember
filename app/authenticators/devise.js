cf7ad09322b78f3363a7ea71edb8fde824723552
mathens <athens_maisie@yahoo.com>
2016-02-23 16:20:37 -0500

    changed authorizer for heroku

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -1,6 +1,6 @@
 import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
 
 export default DeviseAuthenticator.extend({
- serverTokenEndpoint: 'http://localhost:3000/users/sign_in',
+ serverTokenEndpoint: 'https://git.heroku.com/desolate-falls-11389.git',
  identificationAttributeName: 'username'
 });
