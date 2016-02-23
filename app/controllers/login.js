0fb21795b8921ce3bf3b92192b0d4171edb972f2
cindyk09 <cindy_kim88@hotmail.com>
2016-02-23 16:27:06 -0500

    trying to get current user working

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,21 +1,33 @@
 import Ember from 'ember';
-const { service } = Ember.inject;
-const { store } = Ember.inject;
 
 export default Ember.Controller.extend({
-
-session: service('session'),
-store: service("store"),
-
-  session: Ember.inject.service('session'),
-  actions: {
-   authenticate() {
-     let { identification, password } = this.getProperties('identification', 'password');
-     this.get('session').authenticate('authenticator:devise', identification, password).then(()=>{
-       console.log(this.get("session.isAuthenticated"));
-     }).catch((reason) => {
-       this.set('errorMessage', reason.error || reason);
-     });
+ applicationController: Ember.inject.controller('application'),
+ actions: {
+   setCurrentUser(user){
+     this.get("applicationController").set('currentUser', user);
    }
  }
 });
+
+
+// import Ember from 'ember';
+// const { service } = Ember.inject;
+// const { store } = Ember.inject;
+//
+// export default Ember.Controller.extend({
+//
+// session: service('session'),
+// store: service("store"),
+//
+//   session: Ember.inject.service('session'),
+//   actions: {
+//    authenticate() {
+//      let { identification, password } = this.getProperties('identification', 'password');
+//      this.get('session').authenticate('authenticator:devise', identification, password).then(()=>{
+//        console.log(this.get("session.isAuthenticated"));
+//      }).catch((reason) => {
+//        this.set('errorMessage', reason.error || reason);
+//      });
+//    }
+//  }
+// });
