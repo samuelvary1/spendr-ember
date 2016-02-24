7f1fa181adc4e0f61d9cc8afa6d17f91ebb3f955
Sam Vary <sam.vary@gmail.com>
2016-02-24 10:53:32 -0500

    server token endpoint change

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,5 +4,5 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 export default ActiveModelAdapter.extend(DataAdapterMixin, {
   namespace: "api/v1",
   host: "https://desolate-falls-11389.herokuapp.com",
-  authorizer: "authorizer:devise"
+  authorizer: "authorizer:devise" 
 });
--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -1,6 +1,6 @@
 import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
 
 export default DeviseAuthenticator.extend({
- serverTokenEndpoint: 'https://git.heroku.com/desolate-falls-11389.git',
+ serverTokenEndpoint: 'https://desolate-falls-11389.herokuapp.com',
  identificationAttributeName: 'username'
 });
