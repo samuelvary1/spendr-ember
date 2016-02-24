39ace361c9e66994a5a4f0c67b554d8fe766fae9
cindyk09 <cindy_kim88@hotmail.com>
2016-02-23 22:31:00 -0500

    fixing expense helper

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,31 +4,8 @@ export default Ember.Controller.extend({
  applicationController: Ember.inject.controller('application'),
  actions: {
    setCurrentUser(user){
-     debugger;
+
      this.get("applicationController").set('currentUser', user);
    }
  }
 });
-
-
-// import Ember from 'ember';
-// const { service } = Ember.inject;
-// const { store } = Ember.inject;
-//
-// export default Ember.Controller.extend({
-//
-// session: service('session'),
-// store: service("store"),
-//
-//   session: Ember.inject.service('session'),
-//   actions: {
-//    authenticate() {
-//      let { identification, password } = this.getProperties('identification', 'password');
-//      this.get('session').authenticate('authenticator:devise', identification, password).then(()=>{
-//        console.log(this.get("session.isAuthenticated"));
-//      }).catch((reason) => {
-//        this.set('errorMessage', reason.error || reason);
-//      });
-//    }
-//  }
-// });
