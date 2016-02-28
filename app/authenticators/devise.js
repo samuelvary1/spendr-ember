b5e93344d1394f6a5cc5b3f2fe8491d7ac6f552c
Sam Vary <sam.vary@gmail.com>
2016-02-28 16:42:58 -0500

    called the route wrong COME ON SAM

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -2,6 +2,6 @@ import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
 import ENV from '../config/environment'; 
 
 export default DeviseAuthenticator.extend({
- serverTokenEndpoint: ENV.APP.API_HOST + '/users/log_in',
+ serverTokenEndpoint: ENV.APP.API_HOST + '/users/sign_in',
  identificationAttributeName: 'username'
 });
